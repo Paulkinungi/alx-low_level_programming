4c692ea3ff62349662679ff8940c325344245912
Kinungipaul <kinungipaul21@gmail.com>
2023-02-21 00:29:26 +0300

    #include <stdio.h>

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main-Entry point
- * Return:Always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
-	int d;
-	c='a';
-	d = 0
-	while;
-	 (d<10)
-	{putchard(d + '0');
-		d++;
-	}
-	
 
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
